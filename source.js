b63e07b4ba36b461d51d71ff6074588d4c9c7add
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-01-23 19:51:37 +0100

    All different outcomes reproduceable

--- source.js
+++ source.js
@@ -1,7 +1,21 @@
+//Choose one of these:
+
+// 1. (intermediate value).init
+// var Box2D = require('./Box2D')
+// var Kurt = require('./Kurt')
+// var Farmor = require('./Farmor')
+// var uniq = require('uniq')
+
+// -> kurt
+// var Kurt = require('./Kurt')
+// var Box2D = require('./Box2D')
+// var Farmor = require('./Farmor')
+// var uniq = require('uniq')
+
+// -> farmor
 var Kurt = require('./Kurt')
 var Farmor = require('./Farmor')
-var uniq = require('uniq')
 var Box2D = require('./Box2D')
-
+var uniq = require('uniq')
 
 new Farmor().init()
